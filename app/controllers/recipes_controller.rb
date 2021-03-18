b6771db97667725de6c27204828fe769824d9d4c
jinook929 <jinook929@gmail.com>
2021-03-18 05:52:52 -0400

    commit tree 57510c82b8f543adceed15b0eb45a6d8998c8ea1

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,29 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    5.times.each {|i| @recipe.ingredients.build}
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    # binding.pry
+    # @recipe.ingredients.destroy_all(name: "")
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,15 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+
+  # accepts_nested_attributes_for :ingredients#, reject_if: proc { |i| i[:name].blank? }
+
+  def ingredients_attributes=(ingredients_attributes)
+    ingredients_attributes.values.each {|ingredient_attributes|
+      if ingredient_attributes[:name].present?
+        ingredient = Ingredient.find_or_create_by(ingredient_attributes)
+        binding.pry
+        self.ingredients << ingredient
+      end
+    }
+  end
 end
